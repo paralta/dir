bb58ab7e1fbe01ebb93eefd9d6a6510c220f5c9a
Catarina Paralta <clouropa@cisco.com>
2025-06-25 12:42:09 +0100

    demo

--- cli/cmd/sign/options.go
+++ cli/cmd/sign/options.go
@@ -33,6 +33,8 @@ func init() {
 		"OIDC Provider URL")
 	flags.StringVar(&opts.OIDCClientID, "oidc-client-id", client.DefaultOIDCClientID,
 		"OIDC Client ID")
+	flags.StringVar(&opts.OIDCToken, "oidc-token", "",
+		"OIDC ID Token to use for keyless signing. This token is used to authenticate with the OIDC provider and obtain a signing certificate. ")
 	flags.StringVar(&opts.Key, "key", "",
 		"Path to the private key file to use for signing (e.g., a Cosign key generated with a GitHub token). Use this option to sign with a self-managed keypair instead of OIDC identity-based signing.")
 }
